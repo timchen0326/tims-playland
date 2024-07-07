75a3259ae11c2b828da696117eb90b6fdb49b366
Tim Chen <timchen0326ca@gmail.com>
2024-07-07 16:14:44 +0800

    fix

--- src/util/get-image/index.ts
+++ src/util/get-image/index.ts
@@ -3,11 +3,11 @@ import fs from 'fs';
 import path from 'path';
 
 export function getImages(value: string) {
-  const imagesDir = path.join(process.cwd(), `public/images/${value}`); // Adjust the path as needed
+  const imagesDir = path.join(process.cwd(), `public/images/${value}`);
   const fileNames = fs.readdirSync(imagesDir);
 
   const images = fileNames.map(fileName => ({
-    src: `/images/${fileName}`,
+    src: `/images/${value}/${fileName}`, // Include the 'value' parameter in the path
     alt: fileName.replace(/\.[^/.]+$/, ""),
   }));
 
